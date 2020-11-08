740147b2b4ef12504856281f80625c7930e933f2
AlinaUrOk <71850840+AlinaUrOk@users.noreply.github.com>
2020-11-09 00:58:00 +0300

    Add files via upload

---
+++ Laba_3.cpp
@@ -0,0 +1,22 @@
+﻿// Laba_3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+/*19. 	(*) Дано функцію f(x,y). Вивести на екран функцію g(x)=∫_a^b▒〖f(x,y)〗 dy*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "function f(x;y)=x^2-4xy";
+	long double g, Integral, a, b, f1, f2;
+	cout << "enter the limits " << endl;
+	cout << "a:";
+	cin >> a;
+	cout<< "b:";
+	cin >> b;
+	cout << "Integral: x^2 - 4xy^2/2 dy"<< endl;
+	f1 =  4*(pow(b, 2)) /2;
+	f2 =  4*(pow(a, 2)) / 2 ;
+	Integral = f1 - f2;
+	cout << "g(x):x^2-(" << Integral << "x)";
+	return a;
+}
---
+++ Laba_3_.cpp
@@ -0,0 +1,21 @@
+﻿// Laba_3_.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+/*/*12.	Дано різницеве рівняння x_(i+1)=f(i,x_i). Знайти суму перших n його елементів */
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x, i, v, xi, sum=0;
+	cout << "x(i+1)=f(i,xi)  =  x(i+1)=5(i)xi" << endl;
+	cout << "enter the limit:";
+	cin >> v; '\n';
+	cout << "x_i:";
+	cin >> xi;
+	for (i = 0; i < v; i++)
+	{
+		x = 5 * i * xi;
+		cout << "elements:" << x << endl;
+		sum += x;		
+	}
+	cout << "sum:" << sum;
+}
